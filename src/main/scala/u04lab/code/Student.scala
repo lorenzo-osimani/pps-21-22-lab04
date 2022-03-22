166e221f5120c2f5e08f412c0e9dec1f162fef00
Lorenzo <lorenzo.osimani@studio.unibo.it>
2022-03-22 17:12:38 +0100

    Ex 2 Completed

--- src/main/scala/u04lab/code/Student.scala
+++ src/main/scala/u04lab/code/Student.scala
@@ -5,7 +5,7 @@ import List.*
 trait Student:
   def name: String
   def year: Int
-  def enrolling(course: Course): Unit // the student participates to a Course
+  def enrolling(course: Course*): Unit // the student participates to a Course
   def courses: List[String] // names of course the student participates to
   def hasTeacher(teacher: String): Boolean // is the student participating to a course of this teacher?
 
@@ -13,11 +13,21 @@ trait Course:
   def name: String
   def teacher: String
 
+case class StudentImpl(name: String, year: Int = 2017) extends Student:
+  private var coursesSet: List[Course] = Nil()
+
+  override def enrolling(newCourses: Course*) = newCourses.foreach(course =>
+    coursesSet = List.append(Cons(course, Nil()), coursesSet))
+  override def courses: List[String] = List.map(coursesSet)(course => course.name)
+  override def hasTeacher(teacher: String): Boolean = List.contains(List.map(coursesSet)(course => course.teacher), teacher)
+
+case class CourseImpl(name: String, teacher: String) extends Course
+
 object Student:
-  def apply(name: String, year: Int = 2017): Student = ???
+  def apply(name: String, year: Int = 2017): Student = StudentImpl(name, year)
 
 object Course:
-  def apply(name: String, teacher: String): Course = ???
+  def apply(name: String, teacher: String): Course = CourseImpl(name, teacher)
 
 @main def checkStudents(): Unit =
   val cPPS = Course("PPS", "Viroli")
@@ -26,12 +36,9 @@ object Course:
   val s1 = Student("mario", 2015)
   val s2 = Student("gino", 2016)
   val s3 = Student("rino") // defaults to 2017
-  s1.enrolling(cPPS)
-  s1.enrolling(cPCD)
+  s1.enrolling(cPPS, cPCD)
   s2.enrolling(cPPS)
-  s3.enrolling(cPPS)
-  s3.enrolling(cPCD)
-  s3.enrolling(cSDR)
+  s3.enrolling(cPPS, cPCD, cSDR)
   println(
     (s1.courses, s2.courses, s3.courses)
   ) // (Cons(PCD,Cons(PPS,Nil())),Cons(PPS,Nil()),Cons(SDR,Cons(PCD,Cons(PPS,Nil()))))
