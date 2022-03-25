f3f76b8f4ee6fe5ff93af0bb2432f8d18a3f6540
Lorenzo <lorenzo.osimani@studio.unibo.it>
2022-03-25 18:31:57 +0100

    Exercise 3

--- src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
@@ -1,8 +1,29 @@
 package u04lab.polyglot.a01a
 import Logics.*
+import u04lab.polyglot.a01a.Logics.Result
+import scala.util.Random
+import u04lab.code.List
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01a/sol2/ */
-class LogicsImpl(private val size: Int, private val boat: Int) extends Logics:
+class LogicsImpl(private val gridSize: Int, private val boatSize: Int) extends Logics:
+  import List.*
 
-  def hit(row: Int, col: Int) =
-    Result.HIT
+  private val FAILURES = 5
+  private val r = new Random()
+
+  private var hit: List[Tuple] = List.Nil()
+  private val boatRow = r.nextInt(gridSize)
+  private val boatLeftCol = r.nextInt(gridSize - boatSize + 1)
+  private var failures = 0
+
+  println("x = " + this.boatLeftCol + " y = " + this.boatRow)
+
+  def hit(row: Int, col: Int): Logics.Result =
+    if (row == this.boatRow && col >= this.boatLeftCol && col < this.boatLeftCol + boatSize) {
+      this.hit = List.append(Cons((row, col), Nil()), this.hit)
+      return if (List.length(this.hit) == this.boatSize) Result.WON
+      else Result.HIT
+    }
+    this.failures += 1
+    if (this.failures == FAILURES) Result.LOST
+    else Result.MISS
